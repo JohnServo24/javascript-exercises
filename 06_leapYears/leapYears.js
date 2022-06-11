75f8076f4391b4d4d26f44cb60556a7820af94cb
John Servo <jaservo@up.edu.ph>
2022-06-11 11:40:16 +0800

    added more comments

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -16,6 +16,7 @@ const leapYears = num => {
 // And the nubmer must be not divisible by 100
 // Unless if it is divisible by 400
 // (OR If it is divisible by 100, then it must be divisible by 400 as well)
+// OR finds the first truthty value so think of it as a modified IF statement
 
 console.log(leapYears(1996));
 
