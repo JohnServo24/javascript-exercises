3435eb6df82c5016b26624f6567a083004f217e6
John Servo <jaservo@up.edu.ph>
2022-06-11 11:34:17 +0800

    added comments

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,11 +1,20 @@
+// const leapYears = num => {
+//     if(num % 400 == 0) {
+//         return true;
+//     } else if (num % 4 == 0 && num % 100 != 0) {
+//         return true;
+//     }
+//     return false;
+// };
+
+
 const leapYears = num => {
-    if(num % 400 == 0) {
-        return true;
-    } else if (num % 4 == 0 && num % 100 != 0) {
-        return true;
-    }
-    return false;
-};
+    return num % 4 === 0 && (num % 100 !== 0 || num % 400 === 0);
+}
+
+// Return a number that is divisible by 4
+// And the nubmer must be not divisible by 100
+// Unless if it is divisible by 400
 
 console.log(leapYears(1996));
 
