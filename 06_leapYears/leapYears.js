add6fbb4d2a9fb019dbefef20c71b570c0a9f2dc
John Servo <jaservo@up.edu.ph>
2022-06-11 11:34:53 +0800

    edited comment

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -12,7 +12,7 @@ const leapYears = num => {
     return num % 4 === 0 && (num % 100 !== 0 || num % 400 === 0);
 }
 
-// Return a number that is divisible by 4
+// Return true if the num is divisible by 4
 // And the nubmer must be not divisible by 100
 // Unless if it is divisible by 400
 
