281acc965af3e499d17c0f6a26688f1d0b28b95a
John Servo <jaservo@up.edu.ph>
2022-06-11 11:29:46 +0800

    added some comments so that it will stick to my mind

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,24 +1,49 @@
 
 
-const removeFromArray = (arr, ...itemsToRemove) => {
-    for(let i = 0; i < arr.length; i++) {
-        for(let j = 0; j < itemsToRemove.length; j++) {
-            if(arr[i] === itemsToRemove[j]) {
-                delete arr[i];
-            }
+// const removeFromArray = (arr, ...itemsToRemove) => {
+//     for(let i = 0; i < arr.length; i++) {
+//         for(let j = 0; j < itemsToRemove.length; j++) {
+//             if(arr[i] === itemsToRemove[j]) {
+//                 delete arr[i];
+//             }
+//         }
+//     }
+
+//     // let newArray= [];
+//     // for(let i = 0; i < arr.length; i++){
+//     //     if(arr[i] !== undefined) {
+//     //         newArray.push(arr[i]);
+//     //     }
+//     // }
+
+//     return arr.filter(item => {return item !== undefined});
+
+// };
+
+
+const removeFromArray = (...args) => {
+    let array = args[0];
+
+    let newArray = [];
+
+    // If the array items are not in the arguments of the user
+    // Input it to new array
+    // ...args basically gives us an array of values from the user
+    // The value after the first value (args[0]) is the values to be removed from the array
+    // So if the values after args[0] is inside args[0], then we remove them
+    // In this case, we did the reverse.
+    // If the values inside of args[0] is not a value of the values after args[0]
+    // Then we include it to the new array
+    // If the values inside of args[0] cannot be found in the values after args[0],
+    // Then it is a value that shouldn't be removed since the values after args[0] are to be removed
+    array.forEach(items => {
+        if(!args.includes(items)) {
+            newArray.push(items);
         }
-    }
+    });
 
-    // let newArray= [];
-    // for(let i = 0; i < arr.length; i++){
-    //     if(arr[i] !== undefined) {
-    //         newArray.push(arr[i]);
-    //     }
-    // }
-
-    return arr.filter(item => {return item !== undefined});
-
-};
+    return newArray;
+}
 
 let test = [1, 2, 3, 4];
 test = removeFromArray(test, 3, 2);
