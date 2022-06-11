a546497e0f7e11f01a7857f4746e679520c006fa
John Servo <jaservo@up.edu.ph>
2022-06-11 11:06:50 +0800

    finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,13 @@
-const ftoc = function() {
-
+const ftoc = num => {
+  return parseFloat(((5/9) * (num - 32)).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = num => {
+  return parseFloat((((9/5) * num) + 32).toFixed(1));
 };
+
+
+console.log(ctof(0));
 
 // Do not edit below this line
 module.exports = {
