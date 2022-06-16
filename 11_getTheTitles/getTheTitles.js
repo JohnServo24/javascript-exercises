f80f898156b04fe843b022ad2f14a92429927797
John Servo <jaservo@up.edu.ph>
2022-06-16 22:35:30 +0800

    did getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,38 @@
-const getTheTitles = function() {
+// USING map
+// const getTheTitles = arr => {
+//     return arr.map(item => item.title);
+// };
 
+// Without methods
+// const getTheTitles = arr => {
+//     let newArr = [];
+//     arr.forEach(item => newArr.push(item['title']));
+
+//     return newArr;
+// };
+
+// Using regular for loop
+const getTheTitles = arr => {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        newArr.push(arr[i]["title"]);
+    }
+
+    return newArr;
 };
+
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
