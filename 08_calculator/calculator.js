02eef2f363c5aeb7dad76e947e4051677d7832e7
John Servo <jaservo@up.edu.ph>
2022-06-16 20:16:06 +0800

    finished calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,30 @@
-const add = function() {
-	
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+// total -> the accumulator
+// It stores the value of the previous iteration of the loop
+// .reduce is basically a magic for loop
+// the 2nd argument is the initial value of the accumulator
+const sum = arr => {
+  const newArr = arr.reduce((total, item) => total + item, 0);
+
+  return newArr;
 };
 
-const subtract = function() {
-	
+const multiply = arr => {
+  const newArr = arr.reduce((total, item) => total * item, 1);
+
+  return newArr;
 };
 
-const sum = function() {
-	
-};
+const power = (a, b) => a ** b;
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+// I used recursion just like what I've learned in computer science
+// If a = 0, then return 1 since 0! = 1
+// If its not 0, then subtract the number by 1 all the way down
+// and multiply all the way up
+const factorial = a => a === 0 ? 1 : a === 1 ? a : a * factorial(a - 1);
 
 // Do not edit below this line
 module.exports = {
